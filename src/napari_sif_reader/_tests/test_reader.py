ac62e9d1356f7d347e09bf14038135524933054e
Ruben Lopez <rjlopez2@gmail.com>
2023-04-06 21:52:51 +0200

    revert default test

--- src/napari_sif_reader/_tests/test_reader.py
+++ src/napari_sif_reader/_tests/test_reader.py
@@ -1,55 +1,31 @@
 import numpy as np
 
-# from napari_sif_reader import napari_get_reader
-
-# import pytest
-# from napari_sif_reader import _reader
-# from napari_sif_reader import get_image_reader
-# from napari_sif_reader import _reader
-# from skimage.io import imsave
-
-# @pytest.mark.parametrize("ext", _reader.SUPPORTED_IMAGES)
-# def test_get_image_reader(ext):
-#     path = f"fake_path/img{ext}"
-#     assert _reader.napari_get_reader(path) is not None
-#     assert _reader.napari_get_reader([path]) is not None
-
-
-# def test_get_img_reader(tmp_path):
-
-#     path = str(tmp_path / "myfile.sif")
-    
-#     assert napari_get_reader(path) is not None
-#     assert napari_get_reader([path]) is not None
-
-
-def test_something_else():
-    pass
+from napari_sif_reader import napari_get_reader
 
 
 # tmp_path is a pytest fixture
-# def test_reader(tmp_path):
-#     """An example of how you might test your plugin."""
+def test_reader(tmp_path):
+    """An example of how you might test your plugin."""
 
-#     # write some fake data using your supported file format
-#     my_test_file = str(tmp_path / "myfile.npy")
-#     original_data = np.random.rand(20, 20)
-#     np.save(my_test_file, original_data)
+    # write some fake data using your supported file format
+    my_test_file = str(tmp_path / "myfile.npy")
+    original_data = np.random.rand(20, 20)
+    np.save(my_test_file, original_data)
 
-#     # try to read it back in
-#     reader = napari_get_reader(my_test_file)
-#     assert callable(reader)
+    # try to read it back in
+    reader = napari_get_reader(my_test_file)
+    assert callable(reader)
 
-#     # make sure we're delivering the right format
-#     layer_data_list = reader(my_test_file)
-#     assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
-#     layer_data_tuple = layer_data_list[0]
-#     assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
+    # make sure we're delivering the right format
+    layer_data_list = reader(my_test_file)
+    assert isinstance(layer_data_list, list) and len(layer_data_list) > 0
+    layer_data_tuple = layer_data_list[0]
+    assert isinstance(layer_data_tuple, tuple) and len(layer_data_tuple) > 0
 
-#     # make sure it's the same as it started
-#     np.testing.assert_allclose(original_data, layer_data_tuple[0])
+    # make sure it's the same as it started
+    np.testing.assert_allclose(original_data, layer_data_tuple[0])
 
 
-# def test_get_reader_pass():
-#     reader = napari_get_reader("fake.file")
-#     assert reader is None
+def test_get_reader_pass():
+    reader = napari_get_reader("fake.file")
+    assert reader is None
